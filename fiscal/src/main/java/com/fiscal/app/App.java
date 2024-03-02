3700fd9104d422105e2be9ce4cd998d5f7d192f1
Fernando Marques <fernando.silva@ccc.ufcg.edu.br>
2024-03-02 01:28:34 +0000

    FEAT: add menu

--- fiscal/src/main/java/com/fiscal/app/App.java
+++ fiscal/src/main/java/com/fiscal/app/App.java
@@ -1,13 +1,48 @@
 package com.fiscal.app;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        while (choice != 2) {
+            clearScreen();
+            System.out.println("-- Gerador de Nota Fiscal --\n");
+            System.out.println("1. Adicionar fatura");
+            System.out.println("2. Sair \n");
+            System.out.print("Escolha uma opção: ");
+
+            if (scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+                scanner.nextLine(); 
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("\nAdicionando fatura");
+                        scanner.nextLine(); 
+                        break;
+                    case 2:
+                        System.out.println("Saindo");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Tente novamente.");
+                        break;
+                }
+            } else {
+                System.out.println("Entrada inválida. Por favor, insira um número.");
+                scanner.nextLine(); 
+            }
+        }
+        scanner.close();
     }
+
+    private static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }  
 }
